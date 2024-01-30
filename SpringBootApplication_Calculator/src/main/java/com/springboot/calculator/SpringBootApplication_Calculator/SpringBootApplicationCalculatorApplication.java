5002cf625830238bf8d5cf8cfee8bfd52d0f6e9a
AjeethaSampath <nikithasampath007@gmail.com>
2024-01-30 15:06:40 +0000

    Commit

---
+++ SpringBootApplication_Calculator/src/main/java/com/springboot/calculator/SpringBootApplication_Calculator/SpringBootApplicationCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.calculator.SpringBootApplication_Calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationCalculatorApplication.class, args);
+	}
+
+}
---
+++ SpringBootApplication_Calculator/src/test/java/com/springboot/calculator/SpringBootApplication_Calculator/SpringBootApplicationCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.calculator.SpringBootApplication_Calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApplicationCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
